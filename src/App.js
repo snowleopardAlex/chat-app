8dbdaeaae45bc37217759aa64033b714773e7fe6
Alexa Slomski <alexaslomski@Alexas-MacBook-Pro.local>
2020-11-20 22:57:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ChatWindow from './ChatWindow';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const users = [{ username: 'Amy' }, { username: 'Jon' }];
+
+class App extends Component {
+  state = {
+    messages: [], //will hold this --> {usermame: 'Amy', text: 'a'}
+  };
+  onMessage = (username, message) => {
+    const newMessage = {
+      ['username']: username,
+      ['text']: message,
+    };
+    this.setState(currState => ({
+      messages: currState.messages.concat([newMessage]),
+    }));
+  };
+  render() {
+    const { messages } = this.state;
+
+    return (
+      <div className="App">
+        <div className="container">
+          {users.map(user => (
+            <ChatWindow
+              key={user.username}
+              user={user}
+              messages={messages}
+              onMessage={this.onMessage}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
